c3bd0a7f7e94566adb11d6367065bd7ad2a1a3f8
Hila Kornis <hila.kornis2@gmail.com>
2024-07-19 19:08:13 +0300

    Added a few compilation files for visual code

---
+++ main.c
@@ -0,0 +1,17 @@
+#include <stdio.h> 
+
+int main() {
+    // Write C++ code here
+    // std::cout << "hila k ";
+    // int j = 2; 
+    // int count = 5;
+    // for (int i = 0; i < count; i++)
+    // {
+    //     j++;
+    // }
+
+    
+    printf("Hello World!\n");
+
+    return 0;
+}
